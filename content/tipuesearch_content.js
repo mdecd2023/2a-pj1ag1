831ff618cc88ddbc656c73fe1ca2f817fb9b1a2e
shih1717 <41023143@gm.nfu.edu.tw>
2023-03-09 18:44:39 +0800

    revise about

--- content/tipuesearch_content.js
+++ content/tipuesearch_content.js
@@ -1,1 +1,1 @@
-var tipuesearch = {"pages": [{'title': 'About', 'text': 'cmsimde_site \n', 'tags': '', 'url': 'About.html'}, {'title': 'Schedule', 'text': 'week1:建立倉儲及可攜python程式', 'tags': '', 'url': 'Schedule.html'}]};
+var tipuesearch = {"pages": [{'title': 'About', 'text': 'cmsimde_site \n 第一組', 'tags': '', 'url': 'About.html'}, {'title': 'Schedule', 'text': 'week1:建立倉儲及可攜python程式', 'tags': '', 'url': 'Schedule.html'}]};
